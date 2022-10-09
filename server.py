e039b9db86f97cc0a0e1372a697c13bd9db8b326
bree parker <breannaparker1991@gmail.com>
2022-10-08 19:33:12 -0700

    several attemps later

--- server.py
+++ server.py
@@ -13,9 +13,68 @@
 # Print the smallest temperature.
 # Print a message that tells the user how many temperatures are in the list.
 
-temperatures = []
 
-def temps():
-  while True:
-    temperatures.append(int(input("Enter temperature or done:")))
+import main
+
+temps = []
+  
+def add_temps():
+  for i in range():
+    n = input("Enter temperatures or done:")
+    if n == int():
+      temps.append(n) # adding the element
+    elif n == "done":
+      break
+    elif():
+      print("error, please type a valid entry")
+      
+print(temps)
+
+# def add_temps():
+#   temperatures = []
+#   while True:
+#     temps = temperatures.append(input("Enter temperature or done:"))
+#     if temps == 'done':
+#       print(temperatures)
+#       break
+  
+
+# def add_temps():
+#   try: 
+#     temperatures = []
+#     while True:
+#       temps = temperatures.append(int(input("Enter temperature or done:")))
+#   except: #anything except number will exit loop
+#     print(temperatures)
+
+
+
+# creating an empty list
+# def add_temps():
+#   temperatures = []
+#   while True:
+#     if temperatures == done:
+#       break
+#     else:
+#       temperatures = list(map(int,input("\nEnter the tempatures and end with done : ").strip().split()))
+#       print(temperatures)
+    
+
+# for i in range(0, done):
+#     ele = int(input())
+#     temperatures.append(ele) # adding the element
+      
+# print(lst)
+
+add_temps()
+
+
+
+
+
+
+
+# alternative that didn't match criteria
+
+
 
